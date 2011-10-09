1a0515fecb179482ec7e751466dea1d97a363172
Melwyn Pawar <melwynpawar@gmail.com>
2011-10-09 16:54:32 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+gem 'rails', '3.0.10'
+
+
+gem 'sqlite3-ruby', '1.3.3', :require => 'sqlite3'
+
+group :development do
+	gem 'rspec-rails', '2.0.1'
+	gem 'annotate', '2.4.0'
+	gem 'gravatar_image_tag', '0.1.0'
+end
+
+group :test do
+	gem 'rspec', '2.0.1'
+	gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crush::Application.config.secret_token = '6d58903adc613b0db78149d2bf1828abb7738ed405f817f65b74e51b43b14b913fe779ae702040c11a15c36448731e2455319adb37089b2aa06f5ee266e617ad'
