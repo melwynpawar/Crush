8e21aed11b313133449e04bcd614389607637e34
Melwyn Pawar <melwynpawar@gmail.com>
2011-10-15 15:56:12 -0700

    layout links added and tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Crush::Application.routes.draw do
-  get "pages/home"
+  
+  root :to => "pages#home"
+  match '/contact', :to=>'pages#contact'
+  match '/about',:to=>'pages#about'
 
-  get "pages/contact"
 
-  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
